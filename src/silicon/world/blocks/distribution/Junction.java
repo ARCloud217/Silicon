3fb415652cd119f802a54b2599e7406a2f2cc711
Xiaobei09 <Xiaobei09@users.noreply.github.com>
2026-08-23 02:15:39 +0800

    ci: 审核规则改为中文回复，Junction 增加 team 检查

--- src/silicon/world/blocks/distribution/Junction.java
+++ src/silicon/world/blocks/distribution/Junction.java
@@ -99,9 +99,10 @@ public class Junction extends LiquidJunction {
                             continue;
                         }
 
-                        // Transfer item to destination and shift buffer
-                        dest.handleItem(this, item);
-                        System.arraycopy(buffer.buffers[i], 1, buffer.buffers[i], 0, buffer.indexes[i] - 1);
+                    // Transfer item to destination and shift buffer
+                    if (dest.team != team) continue;
+                    dest.handleItem(this, item);
+                    System.arraycopy(buffer.buffers[i], 1, buffer.buffers[i], 0, buffer.indexes[i] - 1);
                         buffer.indexes[i]--;
                     }
                 }
